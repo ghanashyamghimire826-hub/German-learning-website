d8ca87823c622cd4449d390d04c79613d7420493
Syemon <ghanashyamghimire826@gmail.com>
2026-09-02 19:36:20 +0545

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/German-learning-website',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
